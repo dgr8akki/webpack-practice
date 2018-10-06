9e852bf2a7c7c81e32cbde1913ff423a71f891e2
Aakash Pahuja <aakash.pahuja@kony.com>
2018-10-06 19:58:49 +0530

    Add style handling in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
+const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  devServer: {
-    // Display only errors to reduce the amount of output.
-    stats: "errors-only",
+const parts = require("./webpack.parts");
 
-    // Parse host and port from env to allow customization.
-    //
-    // If you use Docker, Vagrant or Cloud9, set
-    // host: options.host || "0.0.0.0";
-    //
-    // 0.0.0.0 is available to all network devices
-    // unlike default `localhost`.
-    host: process.env.HOST, // Defaults to `localhost`
-    port: process.env.PORT, // Defaults to 8080
-    open: true, // Open the page in browser
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack demo",
+      }),
+    ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack demo",
-    }),
-  ],
+  parts.loadCSS(),
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode });
+  }
+
+  return merge(commonConfig, developmentConfig, { mode });
 };
---
+++ webpack.parts.js
@@ -0,0 +1,22 @@
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    stats: "errors-only",
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    open: true,
+    overlay: true,
+  },
+});
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+});
